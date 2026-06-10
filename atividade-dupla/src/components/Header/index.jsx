012b9934441768acf98f686a636b8696414308ad
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-10 20:36:53 -0300

    teste

--- atividade-dupla/src/components/Header/index.jsx
+++ atividade-dupla/src/components/Header/index.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+
 import { Link } from 'react-router-dom';
-import './Header.css';
+import './style.css';
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <header className="site-header">
       <div className="logo">
         <Link to="/">O JANTAR SECRETO</Link>
       </div>
 
-      {/* Menu de Navegação */}
-      <nav className={`nav-menu ${menuOpen ? 'open' : ''}`}>
+      <nav className="nav-menu">
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/regras">Regras</Link></li>
@@ -20,18 +17,10 @@ export default function Header() {
         </ul>
       </nav>
 
-      {/* Área de Ação (Cadastro + Reserva) */}
       <div className="header-actions">
         <Link to="/cadastro" className="link-login">Membro? Entrar</Link>
         <Link to="/reservas" className="btn-convite">Reservar Lugar</Link>
       </div>
-
-      {/* Botão Mobile */}
-      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </button>
     </header>
   );
 }
