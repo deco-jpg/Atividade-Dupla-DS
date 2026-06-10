c774b23c11319cad24cd4b3377cb6c8609eeb569
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-10 20:41:37 -0300

    moorra

--- atividade-dupla/src/App.jsx
+++ atividade-dupla/src/App.jsx
@@ -1,19 +1,16 @@
 import './App.css'
-
+import { BrowserRouter } from 'react-router-dom' // Importe isso aqui!
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 
-
 function App() {
-  
-
   return (
-    <>
-      <Header/>
-      <Footer/>
+    <BrowserRouter>
+      <Header />
       <Home />
-    </>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
