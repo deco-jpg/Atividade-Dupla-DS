bbe64e4aef397708ab3ce41eecaff6caa7f92714
GuilhermeSilva-dev <guilherme.s.souza7@ba.estudante.senai.br>
2026-06-10 20:24:23 -0300

    njhbjjhjh

--- atividade-dupla/src/pages/Home/index.jsx
+++ atividade-dupla/src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="home">
             <h1><strong>Como funciona o jantar secreto?</strong></h1>
-            <p>O jantar secreto é um evento em que cada participante recebe o nome de uma pessoa do grupo, exceto o próprio. A tarefa é encontrar essa pessoa e entregar um presente sem revelar sua identidade.</p>
+            <p>O .</p>
         </div>
     )
 }
